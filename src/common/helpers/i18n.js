1dc435cf4be11dfe4f411921e634ec6a1894b840
pazmiller <a309513511@gmail.com>
2025-04-19 18:49:32 +0800

    gradle and android packages update

--- src/common/helpers/i18n.js
+++ src/common/helpers/i18n.js
@@ -1,5 +1,6 @@
-import { getLocales } from 'react-native-localize';
+import LocalizedStrings from 'react-native-localization';
+// import  { getLocales }  from 'react-native-localize';
 import strings from '../constants/strings';
 
-const i18n = new LocalizedStrings( strings );
+const i18n = new LocalizedStrings(strings);
 export default i18n;
